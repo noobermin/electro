abb279094690e8b387e0dbcacc325d1187b69372
noobermin <ngirmang.1@osu.edu>
2014-12-15 19:09:37 -0500

    updated stuff from noob3d

--- noob3d/numer.hpp
+++ noob3d/numer.hpp
@@ -49,10 +49,10 @@ namespace noob3d
       T 
 	rk1,rk2,rk3,rk4,
 	vk1,vk2,vk3,vk4;
-      rk4_sample(r,             v,             rk1, vk1, dt,       f);
-      rk4_sample(r + rk1*t/2.0, v + vk1*t/2.0, rk2, vk2, t+dt/2.0, f);
-      rk4_sample(r + rk2*t/2.0, v + vk2*t/2.0, rk3, vk3, t+dt/2.0, f);
-      rk4_sample(r + rk3*t,     v + vk3*t,     rk4, vk4, t+dt,     f);
+      rk4_sample(r,              v,              rk1, vk1, t,        f);
+      rk4_sample(r + rk1*dt/2.0, v + vk1*dt/2.0, rk2, vk2, t+dt/2.0, f);
+      rk4_sample(r + rk2*dt/2.0, v + vk2*dt/2.0, rk3, vk3, t+dt/2.0, f);
+      rk4_sample(r + rk3*dt,     v + vk3*dt,     rk4, vk4, t+dt,     f);
       
       v += (vk1/6.0 + vk2/3.0 + vk3/3.0 + vk4/6.0)*dt;
       r += (rk1/6.0 + rk2/3.0 + rk3/3.0 + rk4/6.0)*dt;
